37cc253dcd10ada1105ae87a0371d4c726d6dbe5
napatlnwza <khun.064239@gmail.com>
2025-12-28 19:14:38 +0700

    update

---
+++ Main/src/fighter.java
@@ -0,0 +1,6 @@
+public class fighter {
+    public float hp=100;
+    public int atk=35;
+    public int def=20;
+    public int speed=10;
+}
---
+++ Main/src/tank.java
@@ -0,0 +1,6 @@
+public class tank {
+    public float hp=150;
+    public int atk=20;
+    public int def=45;
+    public int speed=10;
+}
